b30f1c9c9ecfee40afd8111267ed085fd26ab942
Cesar <cesaraug76@hotmail.com>
2022-04-05 11:33:46 -0300

    Finalizando o CRUD

--- crud/index.php
+++ crud/index.php
@@ -1,11 +1,16 @@
 <?php 
+    // Conexão
+    include_once 'php_action/db_connect.php';
     // Header
     include_once 'includes/header.php'; 
+    // Messagem
+    include_once 'includes/message.php';
 ?>
+
 <div class="row">
     <div class="col s12 m6 push-m3">
         <h3 class="light">Clientes</h3>
-        <table classs="striped">
+        <table class="striped">
             <thead>
                 <tr>
                     <th>Nome:</th>
@@ -15,14 +20,39 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th>Cesar</th>
-                    <th>Augusto</th>
-                    <th>cesaraug76@hotmail.com</th>
-                    <th>46</th>
-                    <th><a href="" class="btn-floating blue"><i class="material-icons">edit</i></a></th>
-                    <th><a href="" class="btn-floating red"><i class="material-icons">delete</i></a></th>
-                </tr>     
+                <?php
+                    $sql = "SELECT * FROM clientes";
+                    $resultado = mysqli_query($connect, $sql);
+                    while ($dados = mysqli_fetch_array($resultado)):
+                    $id = $dados['id'];
+                    $nome = $dados['nome']; 
+                    $sobrenome = $dados['sobrenome']; 
+                    $email = $dados['email']; 
+                    $idade = $dados['idade']; 
+                ?>
+                <tr>                    
+                    <td><?=$nome; ?></td>
+                    <td><?=$sobrenome; ?></td>
+                    <td><?=$email; ?></td>
+                    <td><?=$idade; ?></td>
+                    <td><a href="editar.php?id=<?=$id ;?>" class="btn-floating blue"><i class="material-icons">edit</i></a></td>
+                    <td><a href="#modal<?php echo $dados['id']; ?>" class="btn-floating red modal-trigger"><i class="material-icons">delete</i></a></td>
+                    <!-- Modal Structure -->
+                    <div id="modal<?=$id ;?>" class="modal">
+                        <div class="modal-content">
+                        <h4>Opa</h4>
+                        <p>Tem certeza que deseja excluir esse cliente?</p>
+                        </div>
+                        <div class="modal-footer">                        
+                            <form action="php_action/delete.php" method="POST">
+                                <input type="hidden" name="id" value="<?php echo $dados['id']; ?>">
+                                <button type="submit" name="btn-deletar" class="btn red">Sim, quero deletar</button>
+                                <a href="#!" class="modal-close waves-effect waves-green btn-flat">Cancelar</a>
+                            </form>
+                        </div>
+                    </div>                    
+                </tr> 
+                <?php endwhile; ?>    
             </tbody>
         </table>
         <br>
--- crud/php_action/create.php
+++ crud/php_action/create.php
@@ -1,17 +1,31 @@
 <?php
+// Sessão
+session_start();
+// Conexão
 require_once "db_connect.php";
+// Clear
+function clear($input){
+    global $connect;
+    // SQL
+    $var = mysqli_escape_string($connect, $input);
+    // XSS
+    $var = htmlspecialchars($var);
+    return $var;
+}
 
 if (isset($_POST['btn-cadastrar'])):
-    $nome = mysqli_escape_string($connect, $_POST['nome']);
-    $sobrenome = mysqli_escape_string($connect, $_POST['sobrenome']);
-    $email = mysqli_escape_string($connect, $_POST['email']);
-    $idade = mysqli_escape_string($connect, $_POST['idade']);
+    $nome      = clear($_POST['nome']);
+    $sobrenome = clear($_POST['sobrenome']);
+    $email     = clear($_POST['email']);
+    $idade     = clear($_POST['idade']);
 
     $sql = "INSERT INTO clientes (nome, sobrenome, email, idade) VALUES ('$nome', '$sobrenome', '$email', '$idade')"; 
 
     if(mysqli_query($connect, $sql)):
-        header('Location: ../index.php?sucesso');
+        $_SESSION['mensagem'] = "Cadastrado com Sucesso!";
+        header('Location: ../index.php');
     else:
-        header('Location: ../index.php?erro');
+        $_SESSION['mensagem'] = "Erro ao cadastrar!";
+        header('Location: ../index.php');
      endif;
  endif;
